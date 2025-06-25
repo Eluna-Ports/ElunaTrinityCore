420360d2ccdad1f10a495c060728d0e8dafe8d3d
Niam5 <ebakkedahl@gmail.com>
2025-06-25 10:21:33 -0700

    Eluna: Multiple changes
    - Should fix creature events not working due to incorrect preprocessor define
    - Clean up Eluna includes in files that don't have any Eluna functions
    - Add some more hooks
    - Enable disabled hook
    - Timed event rework
    - Eluna Submodule update
    - Add new Eluna config options
    - Remove compatibility mode
    - Rename Map update hook
    - Switch Eluna objects from raw pointers to unique pointers
    - Update Eluna submodule

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -771,7 +771,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
         void UpdateSpawnGroupConditions();
 
 #ifdef ELUNA
-        Eluna* GetEluna() const;
+        Eluna* GetEluna() const { return eluna.get(); }
 
         LuaVal lua_data = LuaVal({});
 #endif
@@ -843,7 +843,7 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
         MPSCQueue<FarSpellCallback> _farSpellCallbacks;
 
 #ifdef ELUNA
-        Eluna* eluna;
+        std::unique_ptr<Eluna> eluna;
 #endif
 
         /*********************************************************/
